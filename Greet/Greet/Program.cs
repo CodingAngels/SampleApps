2dff64c82e4fc9e3d21ba205168dd618e78c5de6
krzysztof-ze-slaska <slazak.krzysztof@gmail.com>
2018-11-08 09:30:11 +0100

    Add Greet application.

---
+++ Greet/Greet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zmienna args zawiera tablicę parametrów przekazanych do programu podczas jego uruchamiania.
+            // Aby wyświetlić pierwszy parametr, odwołujemy się do elementu tablicy o indeksie 0,
+            // ponieważ elementy tablicy numerowane (indeksowane) są od 0.
+            Console.WriteLine("Hello " + args[0]);
+        }
+    }
+}
