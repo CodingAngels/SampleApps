4125312886d58c26ef37a05568512d524a959d9d
krzysztof-ze-slaska <slazak.krzysztof@gmail.com>
2018-12-08 14:29:25 +0100

    Added Decorate application.

---
+++ Zadania/07_Decorate/Decorate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Decorate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decorate(args[0] + " " + args[2]);
+
+            Console.WriteLine("Podaj tekst do udekorowania:");
+            string tekst = Console.ReadLine();
+            Decorate(tekst);
+            Console.ReadKey();
+        }
+
+        static void Decorate(string parametr)
+        {
+            Console.WriteLine("#####" + parametr + "#####");
+        } 
+    }
+}
